fe4d041e5c4b26aee696484ebabcadcd22b25351
prateekbose20011 <smitraprateek2001@gmail.com>
2021-04-03 16:32:08 +0530

    Added Code for the Back-End

---
+++ index.js
@@ -0,0 +1,39 @@
+const jimp = require('jimp')
+const express = require('express')
+const path = require('path')
+const fs = require('fs')
+// const { FontManager } = require('font-manager')
+
+// const theFontMgr = FontManager.instance()
+const app = express()
+
+app.use(express.json())
+app.use(express.static(path.join(__dirname, '/')))
+
+var PORT = process.env.PORT || 3001
+app.listen(PORT)
+console.log("Server Running\n" + path.join(__dirname, '/'))
+
+app.get("/:index", (req, res) => {
+    var text = req.query.text
+    var index = parseInt(req.params.index)
+    console.log(text)
+
+    jimp.read("./images/thumb.jpg")
+        .then(loadedImage => {
+            jimp.loadFont(jimp.FONT_SANS_128_WHITE)
+                .then(font => {
+                    loadedImage.print(font, loadedImage.bitmap.width/50, loadedImage.bitmap.height/100, text)
+                               .write("./images/thumbNew.jpg")
+                    var s = fs.createReadStream(path.join(__dirname, '/images/thumbNew.jpg'))
+                    s.on('open', () => {
+                        res.set('Content-Type', 'image/jpeg')
+                        s.pipe(res)
+                    })
+                    s.on('error', (err) => {
+                        console.log(err)
+                    })
+                })
+        })
+})
+
